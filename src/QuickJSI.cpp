9c877f48294e2b65b17adceecc52039dfea1de8f
Vladimir Morozov <vmorozov@microsoft.com>
2020-07-27 16:35:19 -0700

    Add gtest

--- src/QuickJSI.cpp
+++ src/QuickJSI.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <jsi/jsi.h>
 #include <quickjs-libc.h>
+#include "gtest/gtest.h"
 
-int main()
+TEST(SimpleTest)
 {
-    JSRuntime* rt = JS_NewRuntime();
+  JSRuntime* rt = JS_NewRuntime();
 
-    js_std_init_handlers(rt);
+  js_std_init_handlers(rt);
 
-    JSContext*  ctx = JS_NewContext(rt);
+  JSContext* ctx = JS_NewContext(rt);
 
-    //js_std_loop(ctx);
+  //js_std_loop(ctx);
 
-    std::string code = "let x = 2; x + x;";
+  std::string code = "let x = 2; x + x;";
 
-    JSValue val = JS_Eval(ctx, code.c_str(), code.size(), "filename", 0);
+  JSValue val = JS_Eval(ctx, code.c_str(), code.size(), "filename", 0);
 
-    JS_FreeValue(ctx, val);
+  EXPECT_TRUE(JS_VALUE_GET_TAG(val) == JS_TAG_INT);
+  EXPECT_TRUE(JS_VALUE_GET_INT(val) == 4);
 
-    js_std_free_handlers(rt);
-    JS_FreeContext(ctx);
-    JS_FreeRuntime(rt);
+  JS_FreeValue(ctx, val);
 
-    std::cout << "Hello World!\n";
+  js_std_free_handlers(rt);
+  JS_FreeContext(ctx);
+  JS_FreeRuntime(rt);
 }
